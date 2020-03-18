758b13d5e9ee2f6415ebb9e7609529f68e84501e
mhuen <mhuen@users.noreply.github.com>
2020-03-18 17:47:08 +0100

    setup for travis and code coverage

---
+++ tfscripts/__about__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.0-dev"
+__author__ = "Mirco Huennefeld"
+__author_email__ = "mirco.huennefeld@tu-dortmund.de"
+__description__ = "Collection of TF functions and helpful additions"
+__url__ = "https://github.com/mhuen/TFScripts"
--- version.py
+++
@@ -1,4 +0,0 @@
-"""Contains the package version number
-"""
-
-__version__ = '0.1.0-dev'
