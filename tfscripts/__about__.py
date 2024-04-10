79c08c10d045d421c334357dcb8318878d045434
mhuen <mhuen@users.noreply.github.com>
2024-04-10 14:19:22 +0200

    Update project meta info

--- tfscripts/__about__.py
+++ tfscripts/__about__.py
@@ -1,5 +1,17 @@
-__version__ = "1.0.0-dev"
+__description__ = "Collection of TF functions and helpful additions"
+
+__version_major__ = 1
+__version_minor__ = 0
+__version_patch__ = 0
+__version_info__ = "-dev"
+
+__version__ = "{}.{}.{}{}".format(
+    __version_major__,
+    __version_minor__,
+    __version_patch__,
+    __version_info__,
+)
 __author__ = "Mirco Huennefeld"
 __author_email__ = "mirco.huennefeld@tu-dortmund.de"
 __description__ = "Collection of TF functions and helpful additions"
-__url__ = "https://github.com/mhuen/TFScripts"
+__url__ = "https://github.com/icecube/TFScripts"
--- tfscripts/__init__.py
+++ tfscripts/__init__.py
@@ -1,6 +1,25 @@
-from tfscripts.__about__ import __version__, __description__, __url__
 import tensorflow as tf
 
+from .__about__ import (
+    __version_major__,
+    __version_minor__,
+    __version_patch__,
+    __version_info__,
+    __version__,
+    __description__,
+    __url__,
+)
+
+__all__ = [
+    "__version_major__",
+    "__version_minor__",
+    "__version_patch__",
+    "__version_info__",
+    "__version__",
+    "__description__",
+    "__url__",
+    "FLOAT_PRECISION",
+]
 
 # constants
 FLOAT_PRECISION = tf.float32
